a206d4848f1d6232099bdf38c451700b7de19be7
Polina <32522035+MalinovayaZaya@users.noreply.github.com>
2017-10-28 10:54:58 +0300

    Update 3_2.cpp

--- 3_2.cpp
+++ 3_2.cpp
@@ -3,23 +3,23 @@
 #include <iostream>
 using namespace std;
 int main(){
-int n, s;
-s=0;
-cin >> n;
-int*A=new int[n];
-for(int i=0; i<n; i++){
-    cin >> A[i];
+int N,s=0;
+cin >> N;
+int*A=new int [N];
+for(int i=0; i<N;i++){
+cin >> A[i];}
+for(int i=0;i<N; i++){
+if(A[i]%27==0){
+    if(A[i]%72!=0){
+s+=A[i];
+    }
 }
-for(int i=0; i<n; i++){
-    if(A[i]%27==0){
-        s+=A[i];
-    }
-    else{
-        if(A[i]%72==0){
-            s+=A[i];
-        }
-    }
+else{
+if(A[i]%72==0){
+s+=A[i];}
+}
 }
 cout << s;
+delete []A;
 return 0;
 }
