9c595456dd6331486a0de97b4de4512984c5adda
Polina <32522035+MalinovayaZaya@users.noreply.github.com>
2017-10-28 10:05:04 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n, s;
+s=0;
+cin >> n;
+int*A=new int[n];
+for(int i=0; i<n; i++){
+    cin >> A[i];
+}
+for(int i=0; i<n; i++){
+    if(A[i]%27==0){
+        s+=A[i];
+    }
+    else{
+        if(A[i]%72==0){
+            s+=A[i];
+        }
+    }
+}
+cout << s;
+return 0;
+}
